9102684ea36f90427a4a58aa499d8825acb0f9fd
=omarfaruk <=omarfaruk1.bd@gmail.com>
2025-08-23 12:57:26 +0600

    four operator

---
+++ operator/operator.py
@@ -0,0 +1,67 @@
+# arithmetic operaor [ addition,subtracion,multiplication,division,modulus,power ]
+
+# get user two input and operator
+
+numf = int(input("Enter first number : "))
+nums = int(input("Enter second number : "))
+
+# Arihmetic operator
+# print("Addition numf + nums = ", numf + nums)
+# print("Subtruction numf +- nums = ", numf - nums)
+# print("Multiplication numf * nums = ", numf * nums )
+# print("Division numf / nums = ", numf / nums)
+# print("modulus numf % nums = ", numf % nums)
+# print("Expronential numf ** nums = ", numf ** nums)
+# print("Floor Division numf // nums = ", numf // nums)
+
+# Assignment operator
+# numf += nums
+# print("Addition nmuf =+ nums = ", numf)
+# numf -= nums
+# print("Subtruction numf -=nums + ", numf)
+# numf *= nums
+# print("Multiplication numf *= nums + ", numf)
+# numf /= nums
+# print("Division numf /= nums = ", numf)
+# numf %= nums
+# print("Modulus numf %= nums = ", numf)
+# numf **=nums
+# print("Exponentiation numf **=nums = ", numf)
+# numf //=nums
+# print("Floor division numf //= nums = ", numf)
+
+# Comparison/relational operator >, >=, <, <=, !=, ==
+
+# if numf > nums :
+#     print("number one is grater than nums")
+# elif numf >= nums :
+#     print("Number one is equal or grater than number two")
+# elif numf < nums :
+#     print("Number one is less than number two")
+# elif numf <= nums :
+#     print("Number one is less than or equal number two")
+# elif numf == nums :
+#     print("Number one is equal number two")
+# elif numf != nums :
+#     print("Number one is not equal number two")
+# else :
+#     print("your input is wrong")
+
+# logical operator and , or, not
+
+if numf and nums :
+    print("Both are true")
+if numf or nums :
+    print("Only one is true")
+
+if not numf :
+    print("Number numf is not false")
+
+
+
+
+
+
+
+
+
